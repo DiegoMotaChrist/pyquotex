9eabe170f003c65ca08853f9faea56aaf97a8b2c
Diego Mota Christ <diego.christ@outlook.com>
2023-07-05 21:00:35 -0300

    dev

--- quotexapi/ws/channels/buy.py
+++ quotexapi/ws/channels/buy.py
@@ -1,7 +1,7 @@
 import json
 from pyquotex.quotexapi.ws.channels.base import Base
 from pyquotex.quotexapi.expiration import get_expiration_time
-
+from pyquotex.logger import logger
 
 class Buy(Base):
     """Class for Quotex buy websocket channel."""
@@ -58,7 +58,7 @@ class Buy(Base):
         # }
         # data = f'42["orders/open",{json.dumps(payload)}]'
         # self.send_websocket_request(data)
-
+        logger.info(f'"asset": {asset},"amount": {price},"time": {duration},"action": {direction},"isDemo": {self.api.account_type},"tournamentId": 0,"requestId": {request_id},"optionType": 1')
         self.send_websocket_request(f'42["orders/open",{json.dumps({"asset": asset,"amount": price,"time": duration,"action": direction,"isDemo": self.api.account_type,"tournamentId": 0,"requestId": request_id,"optionType": 1})}]')
 
 
