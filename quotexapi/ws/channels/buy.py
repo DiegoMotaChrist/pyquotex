3e34d34065bea695464bf342831674ca04fec1bc
Diego Mota Christ <diego.christ@outlook.com>
2023-07-07 00:40:47 -0300

    dev

--- quotexapi/ws/channels/buy.py
+++ quotexapi/ws/channels/buy.py
@@ -58,7 +58,6 @@ class Buy(Base):
         # }
         # data = f'42["orders/open",{json.dumps(payload)}]'
         # self.send_websocket_request(data)
-        logger.info(f'"asset": {asset},"amount": {price},"time": {duration},"action": {direction},"isDemo": {self.api.account_type},"tournamentId": 0,"requestId": {request_id},"optionType": 1')
         self.send_websocket_request(f'42["orders/open",{json.dumps({"asset": asset,"amount": price,"time": duration,"action": direction,"isDemo": self.api.account_type,"tournamentId": 0,"requestId": request_id,"optionType": 1})}]')
 
 
